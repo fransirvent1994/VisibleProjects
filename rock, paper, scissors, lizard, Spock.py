8cd5c8e16c6581fa6373e857f1e8831c3e952007
fransirvent1994 <fsirventcandea@gmail.com>
2024-06-02 20:43:26 +0200

    update

--- rock, paper, scissors, lizard, Spock.py
+++ rock, paper, scissors, lizard, Spock.py
@@ -1,17 +1,28 @@
 import random
 
-options = ["paper", "scissors", "rock", "lizard", "spock"]
-user = input(f"Choose option ({options}): ").lower()
-choice = random.choice(options)
-  
-if user == "paper" and choice == "rock" or user == "paper" and choice == "spock" or user == "scissors" and choice == "paper" or user == "scissors" and choice == "lizard" or user == "rock" and choice == "scissors" or user == "rock" and choice == "lizard" or user == "lizard" and choice == "spock" or user == "lizard" and choice == "paper" or user == "spock" and choice == "rock" or user == "spock" and choice == "scissors":
-  print(f"{user} vs {choice}")
-  print("You win!")
+start_game = True
 
-elif user == choice:
-  print("It's a draw")
+while start_game:
+    options = ["paper", "scissors", "rock", "lizard", "spock"]
+    user = input(f"Choose an option ({', '.join(options)}): ").lower()
+    choice = random.choice(options)
 
-else:
-  print(f"{user} vs {choice}")
-  print("You lose")
-  
+    if user == choice:
+        print(f"{user} vs {choice}")
+        print("It's a draw!")
+    elif (
+        (user == "paper" and choice in ["rock", "spock"])
+        or (user == "scissors" and choice in ["paper", "lizard"])
+        or (user == "rock" and choice in ["scissors", "lizard"])
+        or (user == "lizard" and choice in ["spock", "paper"])
+        or (user == "spock" and choice in ["rock", "scissors"])
+    ):
+        print(f"{user} vs {choice}")
+        print("You win!")
+    else:
+        print(f"{user} vs {choice}")
+        print("You lose!")
+
+    play_again = input("Play again? (Y/N): ").lower()
+    if play_again != "y" and play_again != "yes":
+        start_game = False  # End the game loop
