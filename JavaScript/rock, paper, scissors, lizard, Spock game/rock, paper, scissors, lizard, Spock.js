a9d4486e6dc56a350208008a92015fdaa0f6d4ca
fransirvent1994 <fsirventcandea@gmail.com>
2024-06-05 00:08:57 +0200

    update

---
+++ JavaScript/rock, paper, scissors, lizard, Spock game/rock, paper, scissors, lizard, Spock.js
@@ -0,0 +1,27 @@
+
+start_game = True 
+
+while (start_game)
+    options = ["paper", "scissors", "rock", "lizard", "spock"]
+    user = prompt("Choose an option: {', '.join(options)}: ").lower()
+    choice = random.choice(options)
+
+    if user = choice: 
+        print(f"{user} vs {choice}") 
+        print("It's a draw!")
+    elif
+        (user == "paper" and choice in ["rock", "spock"])
+        or (user == "scissors" and choice in ["paper", "lizard"])
+        or (user == "rock" and choice in ["scissors", "lizard"])
+        or (user == "lizard" and choice in ["spock", "paper"])
+        or (user == "spock" and choice in ["rock", "scissors"])
+    )
+        print(f"{user} vs {choice}")
+        print("You win!")
+    else:
+        print(f"{user} vs {choice}")
+        print("You lose!")
+
+    play_again = input("Play again? (Y/N): ").lower()
+    if play_again != "y" and play_again != "yes"
+        start_game = False
