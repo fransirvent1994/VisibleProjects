56d721f968f41ddffa6d0a1e9e5571fd6bf3fdcc
fransirvent1994 <fsirventcandea@gmail.com>
2024-06-05 03:01:33 +0200

    update

--- JavaScript/rock, paper, scissors, lizard, Spock game/rock, paper, scissors, lizard, Spock.js
+++ JavaScript/rock, paper, scissors, lizard, Spock game/rock, paper, scissors, lizard, Spock.js
@@ -1,10 +1,10 @@
 function play(userChoice) {
     const choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    const resultText = document.getElementById('result-text');
+    const resultText = document.getElementById('result_text');
 
     const result = getResult(userChoice, computerChoice);
-    resultText.textContent = `You chose ${userChoice} \n computer chose ${computerChoice}. ${result}`;
+    resultText.textContent = `You chose ${userChoice}. Computer chose ${computerChoice}. ${result}`;
 }
 
 function getResult(userChoice, computerChoice) {
