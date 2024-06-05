2121eaae84bcc955e9de80c974eade49085b608b
fransirvent1994 <fsirventcandea@gmail.com>
2024-06-05 03:08:28 +0200

    update

--- JavaScript/rock, paper, scissors, lizard, Spock game/rock, paper, scissors, lizard, Spock.js
+++ JavaScript/rock, paper, scissors, lizard, Spock game/rock, paper, scissors, lizard, Spock.js
@@ -1,7 +1,7 @@
 function play(userChoice) {
-    const choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
+    const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    const resultText = document.getElementById('result_text');
+    const resultText = document.getElementById("result_text");
 
     const result = getResult(userChoice, computerChoice);
     resultText.textContent = `You chose ${userChoice}. Computer chose ${computerChoice}. ${result}`;
@@ -9,16 +9,16 @@ function play(userChoice) {
 
 function getResult(userChoice, computerChoice) {
     if (userChoice === computerChoice) {
-        return 'It\'s a tie!';
+        return "It´s a tie!";
     }
 
-    if ((userChoice === 'Rock' && (computerChoice === 'Scissors' || computerChoice === 'Lizard')) ||
-        (userChoice === 'Paper' && (computerChoice === 'Rock' || computerChoice === 'Spock')) ||
-        (userChoice === 'Scissors' && (computerChoice === 'Paper' || computerChoice === 'Lizard')) ||
-        (userChoice === 'Lizard' && (computerChoice === 'Spock' || computerChoice === 'Paper')) ||
-        (userChoice === 'Spock' && (computerChoice === 'Scissors' || computerChoice === 'Rock'))) {
-        return 'You win!';
+    if ((userChoice === "Rock" && (computerChoice === "Scissors" || computerChoice === "Lizard")) ||
+        (userChoice === "Paper" && (computerChoice === "Rock" || computerChoice === "Spock")) ||
+        (userChoice === "Scissors" && (computerChoice === "Paper" || computerChoice === "Lizard")) ||
+        (userChoice === "Lizard" && (computerChoice === "Spock" || computerChoice === "Paper")) ||
+        (userChoice === "Spock" && (computerChoice === "Scissors" || computerChoice === "Rock"))) {
+        return "You win!";
     } else {
-        return 'You lose!';
+        return "You lose!";
     }
 }
